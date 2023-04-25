62f6da281fb7e3934fc1c54df4c51e6cf2c4f4b1
Lee Weatherspoon <wowzame2@outlook.com>
2023-04-25 16:26:01 -0400

    Minor adjustments to urls
    Polished a few issues with the settings for allowed hosts and specified some stuff in the urls and views.

--- NotifGrade/NotifGrade/settings.py
+++ NotifGrade/NotifGrade/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)%)%hjv#!t$o$7%5fjcrazuw-q7-==7t^ix&c8ft+qta-7%9w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
--- NotifGrade/home/views.py
+++ NotifGrade/home/views.py
@@ -3,5 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.http import HttpResponse
 # Create your views here.
+#Telling compiler to search for templates in the templates folder
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, templates),'
+)
 def index(request):
-    return HttpResponse('Testing Response')
+    return render(request,"index.html")
