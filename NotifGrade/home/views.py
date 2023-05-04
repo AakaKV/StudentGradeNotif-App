351ae15711f86d2428a7cfbd9e34c01c5e97c7f5
kboring1 <kboring1@ivytech.edu>
2023-05-03 23:10:09 -0400

    Added Assignment form

--- NotifGrade/home/views.py
+++ NotifGrade/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.http import HttpResponse
+from .forms import CreateNewAssignment
 # Create your views here.
 #Telling compiler to search for templates in the templates folder
 TEMPLATE_DIRS = (
@@ -9,3 +10,7 @@ TEMPLATE_DIRS = (
 )
 def index(request):
     return render(request,"index.html")
+
+def assignments(response):
+    form = CreateNewAssignment()
+    return render(response,"assignments.html", {"form":form})
